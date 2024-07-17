7f8f785b21be3477c0b0d7404d5e07e1b896bbc1
KHS_WS_Sam <sam.leguijt@gmail.com>
2024-07-17 16:28:30 +0200

    Changed: - Fixed bug where EntityNamingSystem would get disabled after first frame, which prevented the use of applying a custom name to entities if entities would be created afterwards

--- Assets/Scripts/ECS/Systems/EnemyFollowTargetSystem.cs
+++ Assets/Scripts/ECS/Systems/EnemyFollowTargetSystem.cs
@@ -3,7 +3,6 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public partial class EnemyFollowTargetSystem : SystemBase
 {
--- Assets/Scripts/ECS/Systems/EntityCustomNamingSystem.cs
+++ Assets/Scripts/ECS/Systems/EntityCustomNamingSystem.cs
@@ -1,11 +1,14 @@
+using System.Dynamic;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
 /// This system is used to give Entities that have a <see cref="EntityCustomNameComponent"/> a name visible in the hierarchy. Name value is set in the <see cref="EntityCustomNameComponent.Name"/>
 /// <br/> System gets disabled after applying a name to each entity with the component, which will happen within a few Update frames.
 /// </summary>
+/// 
 public partial class EntityCustomNamingSystem : SystemBase
 {
     protected override void OnCreate()
@@ -15,28 +18,17 @@ public partial class EntityCustomNamingSystem : SystemBase
 
     protected override void OnUpdate()
     {
-        int entitiesQueried = 0;
-        int entitiesApplied = 0;
-
         Entities.WithoutBurst().ForEach((Entity entity, ref EntityCustomNameComponent customName) =>
         {
-            entitiesQueried++;
-
             if (!customName.IsApplied)
             {
                 if (customName.Name.Length > 0)
                 {
-
                     EntityManager.SetName(entity, customName.Name.ToString());
-                    customName.IsApplied = true;
-                    entitiesApplied++;
+                    customName.IsApplied = true;    
                 }
             }
+
         }).Run();
-    
-        if (entitiesQueried > 0 && entitiesApplied == entitiesQueried)
-        {
-            Enabled = false;
-        }
     }
 }
