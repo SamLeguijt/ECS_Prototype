a3dfe4b7a0cc098d0e9e111f56d7b98033c8ca1a
KHS_WS_Sam <sam.leguijt@gmail.com>
2024-07-18 14:17:11 +0200

    Changed: - Added simple camera follow target logic when receiving certain input

--- Assets/Scripts/MonoBehaviour/CameraFollow.cs
+++ Assets/Scripts/MonoBehaviour/CameraFollow.cs
@@ -5,26 +5,24 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Camera cam = null;
-    [SerializeField] private Transform followTarget = null;
-    [SerializeField] private float camSmoothSpeed = .1f;
+    [SerializeField] private Transform target = null;
 
-    private Vector3 startPos;
+    [SerializeField] private KeyCode followKey = KeyCode.Space;
+
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (followTarget == null || cam == null)
-            this.enabled = false;
-
-        startPos = transform.position;  
+        offset = target.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = new Vector3(followTarget.position.x, startPos.y, followTarget.position.z);
-
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * camSmoothSpeed);
-        transform.position = smoothedPosition;
+        if (Input.GetKey(followKey))
+        {
+            transform.position = target.position - offset;
+        }
     }
 }
